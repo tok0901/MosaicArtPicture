ea0de588edd64b1d38d8a4a7603b1186804e9f48
tok0901 <pingyerenxi@gmail.com>
2025-06-25 15:50:13 +0900

    Create Layout_file.cs

---
+++ Layout_file.cs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    android:orientation="vertical"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    >
+    <Button
+        android:text="開始"
+        android:textSize="25.0dp"                           
+        android:layout_width="350.0dp"
+        android:layout_height="300.0dp"
+        android:id="@+id/btnMosaicstart"
+        android:layout_marginTop="10.0dp"
+        />
+</LinearLayout>
